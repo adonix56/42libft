4c1857db4d320e173acd53e3c7cce9416316ac6c
Justin Salanga <jsalanga@e1z3r5p21.42.us.org>
2018-04-23 21:08:31 -0700

    I think this is it!

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: jsalanga <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/18 19:29:04 by jsalanga          #+#    #+#             */
-/*   Updated: 2018/04/23 21:05:16 by jsalanga         ###   ########.fr       */
+/*   Updated: 2018/04/23 21:08:00 by jsalanga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,16 +45,15 @@ char		*ft_itoa(int n)
 	neg = 0;
 	calc_neg(&neg, &nbr);
 	count = digit_count(nbr);
-	str = ft_strnew(count + neg);
-	if (str == NULL)
+	if (!(str = ft_strnew(count + neg)))
 		return (NULL);
-	if (neg)
-		str[0] = '-';
 	while (count > 0)
 	{
 		str[count + neg - 1] = nbr % 10 + '0';
 		count--;
 		nbr /= 10;
 	}
+	if (neg)
+		str[0] = '-';
 	return (str);
 }
