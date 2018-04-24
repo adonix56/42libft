f2f9af0683bd134b8bdfc7bfd180ddf7b023e123
Justin Salanga <jsalanga@e1z3r5p21.42.us.org>
2018-04-23 20:49:55 -0700

    took out neg function

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,15 +6,15 @@
 /*   By: jsalanga <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/18 19:29:04 by jsalanga          #+#    #+#             */
-/*   Updated: 2018/04/23 20:46:07 by jsalanga         ###   ########.fr       */
+/*   Updated: 2018/04/23 20:49:23 by jsalanga         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int	count_digits(int n)
+static int	count_digits(long n)
 {
-	int ret;
+	long ret;
 
 	ret = 1;
 	while (n >= 10)
@@ -25,26 +25,17 @@ static int	count_digits(int n)
 	return (ret);
 }
 
-static void	calc_negs(int *neg, int *n)
-{
-	if (*n < 0)
-	{
-		*neg = 1;
-		*n = -*n;
-	}
-}
 
 char		*ft_itoa(int n)
 {
+	long	nbr;
 	char	*ret;
 	int		neg;
 	int		len;
 
-	if (n == -2147483648)
-		return (ft_strdup("-2147483648"));
-	neg = 0;
-	calc_negs(&neg, &n);
-	len = count_digits(n) + neg;
+	nbr = n;
+	neg = (v < 0 ? 1 : 0);
+	len = count_digits(nbr) + neg;
 	ret = ft_strnew(len);
 	if (ret)
 	{
